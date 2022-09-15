09f7b658ddaacaf8a27c2f97be1aa8d807c800b5
PASCAIL Guillaume <pascailguillaume@gmail.com>
2022-09-15 22:10:28 +0200

    TP Java3 - Boucles - Exercice4

---
+++ src/main/java/tp3Boucle/Exercice4Tablede5.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp3Boucle;
+
+/**
+ *
+ * @author pasca
+ */
+public class Exercice4Tablede5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i =0; i<=9;i++){
+            int resultat = 5 * i;
+            System.out.print("1 * "+ i + " = " + resultat + "   ");
+        }
+    }
+    
+}

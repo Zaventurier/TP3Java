245e86ee62c5b48b83c008f7d9827b2127eb2b9f
PASCAIL Guillaume <pascailguillaume@gmail.com>
2022-09-16 16:42:15 +0200

    Exerice9 - CalculatriceProgramme

--- src/main/java/tp3Boucle/Exerice9Calculatrice.java
+++ src/main/java/tp3Boucle/Exerice9Calculatrice.java
@@ -1,4 +1,38 @@
 package tp3Boucle;
 
+import java.util.Scanner;
+
 public class Exerice9Calculatrice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Saisissez une opération (+,-,/,*) :");
+        char operation = sc.next().charAt(0);
+        boolean quitter = false;
+        float resultat = 0;
+
+        while(!false){
+            if (operation == '+' || operation == '-' || operation == '/' || operation == '*'){
+                System.out.print("Saisir nombre 1 :");
+                float nbr1 = sc.nextInt();
+                System.out.print("Saisir nombre 2 :");
+                float nbr2 = sc.nextInt();
+
+                switch (operation){
+                    case '+':
+                        resultat = nbr1 + nbr2;
+                    case '-':
+                        resultat = nbr1 - nbr2;
+                    case '/':
+                        resultat = nbr1 / nbr2;
+                    case '*':
+                        resultat = nbr1 * nbr2;
+                }
+                System.out.println(resultat);
+                quitter = true;
+            }
+            else {
+                quitter = true;
+            }
+        }
+    }
 }

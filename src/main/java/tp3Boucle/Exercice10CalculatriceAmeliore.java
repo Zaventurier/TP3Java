4b7d38cc3703bbd0b749360f6a3c15123a6913a1
PASCAIL Guillaume <pascailguillaume@gmail.com>
2022-09-16 20:49:20 +0200

    Exerice10 - Calculatrice Amelioré - Ajout de la vérification de la division par 0 (cas erreur)

--- src/main/java/tp3Boucle/Exercice10CalculatriceAmeliore.java
+++ src/main/java/tp3Boucle/Exercice10CalculatriceAmeliore.java
@@ -5,5 +5,44 @@ import java.util.Scanner;
 public class Exercice10CalculatriceAmeliore {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        boolean quitter = false;
+        float resultat = 0;
+
+        while (!false) {
+            System.out.print("Saisissez une opération (+,-,/,*) :");
+            char operation = sc.next().charAt(0);
+            if (operation == '+' || operation == '-' || operation == '/' || operation == '*') {
+                System.out.print("Saisir nombre 1 :");
+                float nbr1 = sc.nextInt();
+                System.out.print("Saisir nombre 2 :");
+                float nbr2 = sc.nextInt();
+
+                switch (operation) {
+                    case '+':
+                        resultat = nbr1 + nbr2;
+                        break;
+                    case '-':
+                        resultat = nbr1 - nbr2;
+                        break;
+                    case '/':
+                        if(nbr2 == 0){
+                            System.out.println("ERREUR : impossible de diviser par 0 !");
+                            break;
+                        }else{
+                            resultat = nbr1 / nbr2;
+                            break;
+                        }
+
+                    case '*':
+                        resultat = nbr1 * nbr2;
+                }
+                System.out.println(resultat);
+                quitter = true;
+            } else {
+                quitter = true;
+                System.out.println("Fin");
+            }
+        }
     }
 }
+

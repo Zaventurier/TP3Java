071216aa37076f45f72bb9da318880caf15dec73
PASCAIL Guillaume <pascailguillaume@gmail.com>
2022-09-17 16:47:37 +0200

    Exercice13 - NombreSecret - modif

--- src/main/java/tp3Boucle/Exercice13NombreSecret.java
+++ src/main/java/tp3Boucle/Exercice13NombreSecret.java
@@ -7,15 +7,21 @@ public class Exercice13NombreSecret {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int secret = new Random().nextInt(100);
-        int choix = 10;
-        int i = 0;
+        int choix = 0;
+        int i = 10;
         while(i != 0){
             System.out.println("Choisir un nombre entre 1 et 100:");
             choix = sc.nextInt();
             if(choix == secret) {
                 System.out.println("Gagné :)");
+                break;
             }else{
-                System.out.println("Perdu :( Reassaye !");
+                if(choix<secret){
+                    System.out.println("Trop petit");
+                }else{
+                    System.out.println("Trop grand");
+                }
+                System.out.println("il reste " + i + "tentatives");
             }
         }
         System.out.println("Perdu ! Le nombre était : " + secret);

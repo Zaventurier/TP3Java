c47f00cd15ac556b79a235ca8ccdb1a2ec672e66
PASCAIL Guillaume <pascailguillaume@gmail.com>
2022-09-19 16:17:12 +0200

    Exercice11Alphabet

--- src/main/java/tp3Boucle/Exercice11Alphabet.java
+++ src/main/java/tp3Boucle/Exercice11Alphabet.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Exercice11Alphabet {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        for (char c = 'a'; c <= 'z'; c++)
+            System.out.println(c);
     }
 }
